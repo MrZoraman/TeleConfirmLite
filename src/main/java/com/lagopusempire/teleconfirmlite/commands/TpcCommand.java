aacee5f03f73992f41d3f5c8990cb1f28338eef7
Matt <MrZoraman@Gmail.com>
2016-04-11 03:22:49 -0600

    other than the messages, the tpc command should be done.

--- src/main/java/com/lagopusempire/teleconfirmlite/commands/TpcCommand.java
+++ src/main/java/com/lagopusempire/teleconfirmlite/commands/TpcCommand.java
@@ -1,19 +1,38 @@
 package com.lagopusempire.teleconfirmlite.commands;
 
+import com.lagopusempire.teleconfirmlite.RequestManager;
+import com.lagopusempire.teleconfirmlite.RequestType;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
 
 /**
  *
  * @author MrZoraman
  */
 public class TpcCommand implements CommandExecutor {
+    private final RequestManager manager;
+    
+    public TpcCommand(RequestManager manager) {
+        this.manager = manager;
+    }
     
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        if(!(src instanceof Player)) {
+            src.sendMessage(Text.of("This command cannot be used by a non-player!"));
+            return CommandResult.success();
+        }
+        
+        Player sender = (Player) src;
+        Player target = args.<Player>getOne("playername").get();
+        manager.request(sender, target, RequestType.GO_THERE);
+        sender.sendMessage(Text.of("Request to teleport to " + target.getName() + " sent."));
+        target.sendMessage(Text.of(sender.getName() + " wants to teleport to you!"));
         
         return CommandResult.success();
     }
