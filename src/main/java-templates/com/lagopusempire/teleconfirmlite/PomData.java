75a837b6eb912d327212e83a962c8e3a65ba5998
Matt <MrZoraman@Gmail.com>
2016-04-11 03:55:33 -0600

    pom cleaned up.

--- src/main/java-templates/com/lagopusempire/teleconfirmlite/PomData.java
+++ src/main/java-templates/com/lagopusempire/teleconfirmlite/PomData.java
@@ -5,5 +5,5 @@ class PomData {
     public static final String NAME = "${project.name}";
     public static final String VERSION = "${project.version}";
     public static final String DESCRIPTION = "${project.description}";
-    public static final String AUTHORS = "${project.properties.authors}";
+    public static final String AUTHORS = "${authors}";
 }
