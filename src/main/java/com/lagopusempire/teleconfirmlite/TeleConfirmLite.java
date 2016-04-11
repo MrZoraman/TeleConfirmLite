47b6fe91cf6864515612f7dca425ef0187d92cc2
Matt <MrZoraman@Gmail.com>
2016-04-11 17:45:36 -0600

    Started on the messages

--- src/main/java/com/lagopusempire/teleconfirmlite/TeleConfirmLite.java
+++ src/main/java/com/lagopusempire/teleconfirmlite/TeleConfirmLite.java
@@ -1,7 +1,19 @@
 package com.lagopusempire.teleconfirmlite;
 
 import com.google.inject.Inject;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Level;
+import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
@@ -26,16 +38,45 @@ import org.spongepowered.api.text.Text;
         version = PomData.VERSION)
 public class TeleConfirmLite {
 
-    // These are all injected on plugin load for users to work from
-    @Inject private Logger logger;
-    // Give us a configuration to work from
-//    @Inject @DefaultConfig(sharedRoot = true) private ConfigurationLoader<CommentedConfigurationNode> configLoader;
-    @Inject private Game game;
+//    // These are all injected on plugin load for users to work from
+//    @Inject private Logger logger;
+//    // Give us a configuration to work from
+////    @Inject @DefaultConfig(sharedRoot = true) private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+//    @Inject private Game game;
+    
+//    @Inject
+//    @DefaultConfig(sharedRoot = false)
+//    private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+    
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private Path privateConfigDir;
     
     private final RequestManager requestManager = new RequestManager();
 
     @Listener
     public void onPreInit(GamePreInitializationEvent event) {
+        Path pluginDir = privateConfigDir.getParent();
+        File pluginPath = pluginDir.toFile();
+        File messagesConfFile = new File(pluginPath, "messages.conf");
+        URL jarMessages = this.getClass().getResource("messages.conf");
+        
+        ConfigurationLoader<CommentedConfigurationNode> messagesConf = HoconConfigurationLoader.builder()
+                .setPath(Paths.get(messagesConfFile.toURI()))
+                .setURL(jarMessages)
+                .build();
+        
+        try {
+            ConfigurationNode rootNode = messagesConf.load();
+            Map<Object, ? extends ConfigurationNode> children = rootNode.getChildrenMap();
+            System.out.println("thing: " + children.get("thing").getString());
+            System.out.println("thing2: " + children.get("thing2").getString());
+//            System.out.println("string: " + rootNode.getString());
+//            System.out.println("rootNode: " + rootNode);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
         new CommandRegistrar().registerCommands(this, requestManager);
     }
 }
