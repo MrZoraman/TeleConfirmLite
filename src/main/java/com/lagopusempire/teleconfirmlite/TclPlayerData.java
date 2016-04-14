5c83b4970a8ec730594f7e24edf718b8f3814d9a
Matt <MrZoraman@Gmail.com>
2016-04-13 21:24:29 -0600

    Timeouts working

--- src/main/java/com/lagopusempire/teleconfirmlite/TclPlayerData.java
+++ src/main/java/com/lagopusempire/teleconfirmlite/TclPlayerData.java
@@ -3,9 +3,12 @@ package com.lagopusempire.teleconfirmlite;
 import org.spongepowered.api.world.Location;
 
 public class TclPlayerData {
+    private static final int TIMEOUT = 10;
+    
     private Location priorLoc = null;
     private RequestDetails requestDetails = null;
     private boolean acceptingRequests = true;
+    private long requestStartTime = -1;
     
     public void toggleAcceptingRequests() {
         acceptingRequests = !acceptingRequests;
@@ -20,11 +23,20 @@ public class TclPlayerData {
     }
 
     public RequestDetails getRequestDetails() {
+        long now = System.currentTimeMillis() / 1000;
+        if(requestStartTime + TIMEOUT < now) {
+            setRequestDetails(null);
+        }
+        
         return requestDetails;
     }
 
     public void setRequestDetails(RequestDetails details) {
         this.requestDetails = details;
+        
+        if(details != null) {
+            requestStartTime = System.currentTimeMillis() / 1000;
+        }
     }
 
     public boolean isAcceptingRequests() {
