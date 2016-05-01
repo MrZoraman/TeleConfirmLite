56c85b5c5f60338f524d23fa8c60f0967e158702
Matt <MrZoraman@Gmail.com>
2016-04-30 20:07:47 -0600

    some cleanup.

--- src/main/java/com/lagopusempire/teleconfirmlite/Utils.java
+++ src/main/java/com/lagopusempire/teleconfirmlite/Utils.java
@@ -6,18 +6,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Utils {
-
-    /**
-     * Export a resource embedded into a Jar file to the local file path.
-     *
-     * @param resourceName ie.: "/SmartLibrary.dll"
-     * @return The path to the exported resource
-     * @throws Exception
-     */
     public static void ExportResource(Class c, String resourceName, File exportLocation) throws Exception {
         InputStream stream = null;
         OutputStream resStreamOut = null;
-        String jarFolder;
         try {
             stream = c.getResourceAsStream(resourceName);//note that each / is a directory down in the "jar tree" been the jar the root of the tree
             if (stream == null) {
@@ -26,7 +17,6 @@ public class Utils {
 
             int readBytes;
             byte[] buffer = new byte[4096];
-//            jarFolder = new File(c.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath().replace('\\', '/');
             resStreamOut = new FileOutputStream(exportLocation + File.separator + resourceName);
             while ((readBytes = stream.read(buffer)) > 0) {
                 resStreamOut.write(buffer, 0, readBytes);
