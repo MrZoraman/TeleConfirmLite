aaea91a8630708ee094ce9aedc652ce6fc3bcfce
Matt <MrZoraman@Gmail.com>
2016-04-30 21:30:30 -0600

    cleanup

--- src/main/java/com/lagopusempire/teleconfirmlite/Utils.java
+++ src/main/java/com/lagopusempire/teleconfirmlite/Utils.java
@@ -2,37 +2,21 @@ package com.lagopusempire.teleconfirmlite;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Utils {
-    public static void ExportResource(Class c, InputStream resourceStream, File exportLocation) throws Exception {
+    public static void ExportResource(InputStream resourceStream, File exportLocation) throws IOException {
         if(resourceStream == null) {
             throw new IllegalArgumentException("ResourceStream cannot be null!");
         }
         
-//        InputStream stream = null;
-        OutputStream resStreamOut = null;
-        try {
-//            stream = c.getResourceAsStream(resourceName);//note that each / is a directory down in the "jar tree" been the jar the root of the tree
-//            if (stream == null) {
-//                throw new Exception("Cannot get resource \"" + resourceName + "\" from Jar file.");
-//            }
-
+        try(OutputStream resStreamOut = new FileOutputStream(exportLocation)) {
             int readBytes;
             byte[] buffer = new byte[4096];
-            resStreamOut = new FileOutputStream(exportLocation);
             while ((readBytes = resourceStream.read(buffer)) > 0) {
                 resStreamOut.write(buffer, 0, readBytes);
-            }
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-//            if (stream != null) {
-//                stream.close();
-//            }
-            if (resStreamOut != null) {
-                resStreamOut.close();
             }
         }
     }
