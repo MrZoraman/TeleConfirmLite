5c682f43e70d560b0a514e68fe35ae8f558b37a5
Matt <MrZoraman@Gmail.com>
2016-04-30 21:42:35 -0600

    messages.yml re-organization

--- src/main/java/com/lagopusempire/teleconfirmlite/messages/Messages.java
+++ src/main/java/com/lagopusempire/teleconfirmlite/messages/Messages.java
@@ -2,20 +2,20 @@ package com.lagopusempire.teleconfirmlite.messages;
 
 public enum Messages {
     PLAYER_ONLY("player_only"),
-    SENDER_REQUEST_TO("sender_request_to"),
-    TARGET_REQUEST_TO("target_request_to"),
-    SENDER_REQUEST_HERE("sender_request_here"),
-    TARGET_REQUEST_HERE("target_request_here"),
+    SENDER_REQUEST_TO("request.to.sender"),
+    TARGET_REQUEST_TO("request.to.target"),
+    SENDER_REQUEST_HERE("request.here.sender"),
+    TARGET_REQUEST_HERE("request.here.target"),
     NO_PENDING_REQUESTS("no_pending_request"),
     TARGET_OFFLINE("target_offline"),
-    SENDER_REQUEST_DENIED("sender_request_denied"),
-    TARGET_REQUEST_DENIED("target_request_denied"),
+    SENDER_REQUEST_DENIED("request.denied.sender"),
+    TARGET_REQUEST_DENIED("request.denied.target"),
     REQUEST_CLEARED("request_cleared"),
-    TARGET_ALREADY_HAS_REQUEST("target_already_has_request"),
-    SENDER_ALREADY_HAS_REQUEST("sender_already_has_request"),
+    TARGET_ALREADY_HAS_REQUEST("request.already_has.target"),
+    SENDER_ALREADY_HAS_REQUEST("request.already_has.sender"),
     SENDER_NOW_ACCEPTING_REQUESTS("sender_now_accepting_requests"),
-    SENDER_NO_LONGER_ACCEPTING_REQUESTS("sender_no_longer_accepting_requests"),
-    TARGET_NOT_ACCEPTING_REQUESTS("target_not_accepting_requests"),
+    SENDER_NO_LONGER_ACCEPTING_REQUESTS("sender_accepting_requests.no"),
+    TARGET_NOT_ACCEPTING_REQUESTS("sender_accepting_requests.yes"),
     NO_PRIOR_LOC("no_prior_loc");
     
     private String key;
